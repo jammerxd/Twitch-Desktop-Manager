f84afa21d6cc245705dee63b03d93fdc740db6a2
git <josh.menzel@yahoo.com>
2015-11-30 17:47:09 -0600

    Initial

---
+++ Twitch Desktop Manager/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PropertyChanged;
+namespace Twitch_Desktop_Manager
+{
+    [ImplementPropertyChanged]
+    public class ViewModel
+    {
+        IMainWindowCallbacks mainWindowCallbacks = null;
+        public string windowTitle { get; set; }
+
+        public ViewModel(IMainWindowCallbacks mainCallbacks)
+        {
+            if (mainCallbacks == null)
+            {
+                throw new ArgumentException("mainwindow callbacks can't be null");
+            }
+            mainWindowCallbacks = mainCallbacks;
+            windowTitle = "Twitch_Desktop_Manager";
+        }
+    }
+}
