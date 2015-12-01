28d287564f1af9d8b7abeec19c56d4280dad4ef9
git <josh.menzel@yahoo.com>
2015-11-30 18:28:16 -0600

    Added icon and purple theme

--- Twitch Desktop Manager/ViewModel.cs
+++ Twitch Desktop Manager/ViewModel.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PropertyChanged;
+using System.Reflection;
+using System.Diagnostics;
 namespace Twitch_Desktop_Manager
 {
     [ImplementPropertyChanged]
     public class ViewModel
     {
         IMainWindowCallbacks mainWindowCallbacks = null;
+
+
+        //--------------VARIABLES--------------\\
+        #region Variables
         public string windowTitle { get; set; }
+        public string version { get; set; }
+        #endregion
+        //--------------END OF VARIABLES--------------\\
 
         public ViewModel(IMainWindowCallbacks mainCallbacks)
         {
@@ -19,7 +28,8 @@ namespace Twitch_Desktop_Manager
                 throw new ArgumentException("mainwindow callbacks can't be null");
             }
             mainWindowCallbacks = mainCallbacks;
-            windowTitle = "Twitch_Desktop_Manager";
+            version = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion.ToString();
+            windowTitle = "Twitch Desktop Manager v" + version;
         }
     }
 }
